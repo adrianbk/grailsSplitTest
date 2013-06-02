713d3f9946f81ef0fdfc7586458333465a7a16e5
Adrian Kelly <adrianbkelly@gmail.com>
2013-06-02 23:11:23 +1000

    VMS now created with java 7 and grails 2.2.2

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-Steps: 
+# Grails Split Test
 
+## Intro (All WIP)
+
+As a grails project grows, the number of tests and execution times increase. Longer execution on developer environments become painful and result in delayed feedback - particularly when the general consensus is to run entire test suites on developer environments before pushing code to a CI environment.
+To alleviate this, running tests in parallel on developer environments can reduce test execution time. It is possible to run tests in parallel by creating multiple operating system processes however, this can become complicated when it comes to resources shared among tests running in parallel - consider the following local resources which are commonly shared by tests running in parallel: 
+* A shared server that stubs out some external service - used by functional tests 
+* Databases - Integration and functional tests
+
+All leading to potential race conditions. Along with race conditions, it also becomes cumbersome to replicate such resources on developer environments. 
+
+This project proides:
+
+1. A base vagrant project used to spin up a configurable number of virtual machines (oracle vitual box) on which tests can be run in parallel.
+- Provisioned using puppet, each VM has a grails stack can be easily provisioned with datastores and stub servers.
+- Mounts the top level projectory on to each VM 
+
+### Why Vagrant 
+	- portable - runs on any development OS
+	- Several provisioning options - puppet, chef sole, etc
+	- Supports several virtualization technoloies (VmWare, VirtualBox, Cloud)
+	- One command to spin up everything
+
+Yes, virtual machines have performance drawbacks but HARDWARE is cheap - developer time is not!
+
+2. A grails plugin which can split out grails tests and test phases depending on the number of machines created by vagrant. 
+	
+
+## Dependancies
+1. Oracle virtual box 4.2.12 (https://www.virtualbox.org/wiki/Downloads)
+2. Vagrant 2 (http://docs.vagrantup.com/v2/installation/)
+3. A Vagrant base box (http://www.vagrantbox.es/)
+- Ubuntu Ubuntu 12.04 LTS x86_64: https://s3-us-west-2.amazonaws.com/squishy.vagrant-boxes/precise64_squishy_2013-02-09.box
+
+## Prerequisites 
 - install vagrant
 - cd gitSplitTest\vagrantProject
 
@@ -7,7 +40,11 @@ Steps:
 -- Copy https://s3-us-west-2.amazonaws.com/squishy.vagrant-boxes/precise64_squishy_2013-02-09.box to gitSplitTest\vms
 -- $ vagrant box add grailsdev ..\vms\precise64_squishy_2013-02-09.box
 
-- vagrant up
+
+## Usefull Vagrant commands
+-Running vagrant
+-- Start up the VMs
+-- $ vagrant up
 
 - Destroying all traces of the vms
 vagrant destroy -f 
--- vagrantProject/manifests/default.pp
+++ vagrantProject/manifests/default.pp
@@ -3,45 +3,116 @@ group { "puppet":
 	ensure => "present", 
 }
 
-#Need to set the path
+#Need to set the path so puppet can resolve common os commands
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
-#exec { 'apt-get update':
-#  command => '/usr/bin/apt-get update',
-#}
 
-#exec { 'apt-get -y dist-upgrade':
-#  command => '/usr/bin/apt-get -y dist-upgrade',
-#}
+exec {
+  'apt_update': 
+    command => 'apt-get update';
+}
 
-#Some OS specific stuff
+#Puppet will automatically guess the packaging format that you are using based on the platform you are on, but you can override it using the provider parameter
+package { ['wget', 'unzip']:
+       ensure=>'installed',
+}
+
+#Example using some OS specific stuff
 #case $operatingsystem {
 #      centos, redhat: { $apache = "httpd" }
 #      debian, ubuntu: { $apache = "apache2" }
 #      default: { fail("Unrecognized operating system for webserver") }
 #    }
 #}
-
 #package {'apache':
 #      name   => $apache,
 #      ensure => installed,
 #}
 
+#Grails aplication stack
+class grailsStack {
+      class { 'java': version => '7u21-0~webupd8~0', }
+      class { 'grails': version => '2.2.2', destination => '/opt',}
+}
 
-class appServer {
-     grails { "grails-2.2.2":
-        version => '2.2.2',
-        destination => '/opt',
+
+#Installs java
+class java($version = '7u21-0~webupd8~0') {
+  package { "python-software-properties": }
+  exec { "add-apt-repository-oracle":
+    command => "/usr/bin/add-apt-repository -y ppa:webupd8team/java",
+    notify => Exec["apt_update"]
+  }
+  exec {
+    'set-licence-selected':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
+    'set-licence-seen':
+      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
+    'set-java-home':
+      command => 'echo \'export JAVA_HOME="/usr/lib/jvm/java-7-oracle/"\' > /etc/profile.d/java_home.sh'
+  }
+  package { 'oracle-java7-installer':
+    ensure  => "${version}",
+    require => [Exec['add-apt-repository-oracle'], Exec['set-licence-selected'], Exec['set-licence-seen']],
+    before  => Exec['set-java-home'],
+  }
+}
+
+
+class grails($version, $destination, $user = "root"){
+
+  if !defined(File["$destination"]) {
+    file { "$destination":
+      ensure => 'directory',
+      owner => "$user",
+      group => "$user",
+      mode => '0755'
     }
+  }
+
+  if "$version" =~ /^1\.3\.[6-9]|^2\..*/ {
+     $base_url = "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS"
+  }
+  else {
+     $base_url = "http://dist.codehaus.org/grails"
+  }
+  notify { "The base url for grails $version is '$base_url'": }
+
+  file { 'download-grails':
+    name          => "$destination/grails-$version.zip",
+    ensure        => "present",
+    source        => "/vagrant/software/grails-$version.zip",
+    owner         => "$user",
+    group         => "$user",
+    mode          => '0755',
+    before        => Exec["unzip-grails"],
+  }
+  #TODO remove the above file resource and replace with wget OR have an option for either
+ # exec{ "download-grails" :
+  #  command => "/usr/bin/wget -d --output-document=$destination/grails-$version.zip $base_url/grails-$version.zip",
+  #  before => Exec["unzip-grails"],
+  #}
+
+  exec{ "unzip-grails" :
+    cwd => "$destination",
+    command => "unzip grails-$version.zip",
+    creates => "$destination/grails-$version",
+    before => Exec["install-grails-$version"],
+  }
+
+  exec{ "set-grails-path" :
+      command => 'echo \'export GRAILS_OPTS="-server -noverify -Xshare:off -Xms512M -Xmx1536M -XX:PermSize=512M"\' > /etc/profile.d/grails_opts.sh',
+  }
+
+  exec { "install-grails-$version":
+    command => "unzip grails-$version.zip",
+    cwd => "$destination",
+    creates => "$destination/grails-$version",
+    before => Exec["set-grails-path"],
+  }
 }
 
-notify { 'Installing required OS packages.': }
 node default {
-	include appServer
+	include grailsStack
 }
-#package { ['git-core', 'openssh-server', 'fabric', 'python-pip', 'vnc4server', 'imagemagick', 'optipng', 'groovy', 'ia32-libs']:
- #      provider=>'apt',
- #      ensure=>'installed',
-#}
 
 
-notify { 'Finished installing required OS packages.': }
