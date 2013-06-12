a578faf7acf18904c170f30228d5348c71273e1f
Adrian Kelly <adrianbkelly@gmail.com>
2013-06-13 00:28:12 +1000

    Using gvm to install grails

--- vagrantProject/manifests/default.pp
+++ vagrantProject/manifests/default.pp
@@ -12,9 +12,9 @@ stage { 'pre':
 }
 
 #Stages the class preConfig to run in the pre stage
-class { 'preConfig':
-  stage => 'pre'
-}
+#class { 'preConfig':
+ # stage => 'pre'
+#}
 
 #Puppet will automatically guess the packaging format that you are using based on the platform you are on, but you can override it using the provider parameter
 package { ['wget', 'unzip', 'git-core', 'openssh-server', 'curl']:
@@ -35,7 +35,7 @@ package { ['wget', 'unzip', 'git-core', 'openssh-server', 'curl']:
 
 #Grails aplication stack
 class grailsStack {
-      class { 'java': version => '7u21-0~webupd8~0', }
+     # class { 'java': version => '7u21-0~webupd8~0', }
       class { 'grails': version => '2.2.2', destination => '/opt',}
 }
 
@@ -66,77 +66,20 @@ class java($version = '7u21-0~webupd8~0') {
 
 
 class grails($version, $destination, $user = "root"){
-
-  if !defined(File["$destination"]) {
-    file { "$destination":
-      ensure => 'directory',
-      owner => "$user",
-      group => "$user",
-      mode => '0755'
-    }
+  exec {'install-gvm':
+    command => 'curl -s get.gvmtool.net | bash',
   }
 
-  if "$version" =~ /^1\.3\.[6-9]|^2\..*/ {
-     $base_url = "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS"
+  exec {'source-gvm':
+    #dont know why we have to use bash -c
+    command => 'bash -c source ~/.gvm/bin/gvm-init.sh',  
   }
-  else {
-     $base_url = "http://dist.codehaus.org/grails"
-  }
-  notify { "The base url for grails $version is '$base_url'": }
-
-  file { 'download-grails':
-    name          => "$destination/grails-$version.zip",
-    ensure        => "present",
-    source        => "/vagrant/software/grails-$version.zip",
-    owner         => "$user",
-    group         => "$user",
-    mode          => '0755',
-   # before        => Exec["unzip-grails"],
-  }
-  #TODO remove the above file resource and replace with wget OR have an option for either
- # exec{ "download-grails" :
-  #  command => "/usr/bin/wget -d --output-document=$destination/grails-$version.zip $base_url/grails-$version.zip",
-  #  before => Exec["unzip-grails"],
-  #}
-
-  exec{ "unzip-grails" :
-    cwd => "$destination",
-    command => "unzip grails-$version.zip",
-    creates => "$destination/grails-$version",
-  #  before => Exec["install-grails-$version"],
-  }
-
-  
-
-  exec { "install-grails-$version":
-    command => "unzip grails-$version.zip",
-    cwd => "$destination",
-    creates => "$destination/grails-$version",
-   # before => Exec["set-grails-opts"],
-  }
-
-  exec{ "set-grails-opts" :
-      command => 'echo \'export GRAILS_OPTS="-server -noverify -Xshare:off -Xms512M -Xmx1536M -XX:PermSize=512M"\' > /etc/profile.d/grails_opts.sh',
-  }
-
-#exec { "set-grails-home":
- # command => 'echo \'export GRAILS_HOME="-server -noverify -Xshare:off -Xms512M -Xmx1536M -XX:PermSize=512M"\' > /etc/profile.d/grails_home.sh',
-#}
-
-file { "/etc/profile.d/grails_home.sh" :
-  content => "export GRAILS_HOME=$destination/$grails-$version",
-}
-
- File['download-grails'] -> Exec["unzip-grails"] -> Exec["install-grails-$version"] -> Exec['set-grails-opts']
- exec{"install-gvm":
-    command => 'curl -s get.gvmtool.net | bash'
- }
- exec{"install-grails":
-  command => 'gvm install grails 2.2.1'
-
- }
  
- Exec['install-gvm'] -> Exec['install-grails']
+  exec { "install-grails":
+    command => "bash -c gvm install grails $version",
+  }
+ 
+  Exec['install-gvm'] -> Exec['source-gvm'] -> Exec['install-grails']
 }
 
 node default {
