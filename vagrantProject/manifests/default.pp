3010f1eabf97b2d34f66a4c0b9037445f6b28969
Adrian Kelly <adrianbkelly@gmail.com>
2013-06-18 00:27:01 +1000

    Java, grails with GRAILS_OPTS , git all installing

--- vagrantProject/manifests/default.pp
+++ vagrantProject/manifests/default.pp
@@ -3,22 +3,29 @@ group { "puppet":
 	ensure => "present", 
 }
 
-#Set the dfaults for all execs
+#Set the defaults for all exec commands
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ], logoutput => 'true' }
 
-# create a new run stage to ensure certain modules are included first
-stage { 'pre':
-  before => Stage['main']
+#Create three stages and define the resource order (main is the default included stage)
+#OS type stuff and OS packages
+stage { 'first': }
+# last stage should cleanup/ do something unusual
+stage { 'last': }
+
+# Stage ordering
+Stage['first'] -> Stage['main'] -> Stage['last']
+
+#TODO uncomment to allow dist upgrade
+#Stages the class preConfig to run in the pre stage
+class { 'preConfig':
+ stage => 'first'
 }
 
-#Stages the class preConfig to run in the pre stage
-#class { 'preConfig':
- # stage => 'pre'
-#}
-
-#Puppet will automatically guess the packaging format that you are using based on the platform you are on, but you can override it using the provider parameter
-package { ['wget', 'unzip', 'git-core', 'openssh-server', 'curl']:
-       ensure=>'installed',
+class installOsPackages {
+	#Puppet will automatically guess the packaging format that you are using based on the platform you are on, but you can override it using the provider parameter
+	package { ['wget', 'unzip', 'git-core', 'openssh-server', 'curl']:
+	   ensure=>'installed',
+	}
 }
 
 #Example using some OS specific stuff
@@ -35,56 +42,111 @@ package { ['wget', 'unzip', 'git-core', 'openssh-server', 'curl']:
 
 #Grails aplication stack
 class grailsStack {
-     # class { 'java': version => '7u21-0~webupd8~0', }
-      class { 'grails': version => '2.2.2', destination => '/opt',}
+	class {'installOsPackages':
+		stage => first,
+	}
+	class { 'java':
+	    version => '7u21-0~webupd8~0',
+	    stage => main,
+	}
+
+	class { 'grails':
+		version => '2.2.2',
+		destination => '/opt',
+		stage => main,
+	}
 }
 
 
 #Installs java
 class java($version = '7u21-0~webupd8~0') {
-  package { "python-software-properties": }
-  exec { "add-apt-repository-oracle":
-    command => "/usr/bin/add-apt-repository -y ppa:webupd8team/java",
-    notify => Exec["apt_update"]
+  package { 'python-software-properties': }
+  exec { 'add-apt-repository-oracle':
+	command => "/usr/bin/add-apt-repository -y ppa:webupd8team/java",
   }
   exec {
-    'set-licence-selected':
-      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
-    'set-licence-seen':
-      command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
-    'set-java-home':
-      command => 'echo \'export JAVA_HOME="/usr/lib/jvm/java-7-oracle/"\' > /etc/profile.d/java_home.sh';
-    'apt_update':
-      command => 'apt-get update';
+	'set-licence-selected':
+	  command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
+	'set-licence-seen':
+	  command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
+	'set-java-home':
+	  command => 'echo \'export JAVA_HOME="/usr/lib/jvm/java-7-oracle/"\' > /etc/profile.d/java_home.sh';
+	'apt_update':
+	  command => 'apt-get update';
   }
   package { 'oracle-java7-installer':
-    ensure  => "${version}",
-    require => [Exec['add-apt-repository-oracle'], Exec['set-licence-selected'], Exec['set-licence-seen']],
-    before  => Exec['set-java-home'],
-  }
+	ensure  => "${version}",
+    }
+
+  Package['python-software-properties'] -> Exec['add-apt-repository-oracle'] -> Exec["apt_update"] -> Exec['set-licence-selected']  -> Exec['set-licence-seen']
+  -> Exec['set-java-home']  -> Package['oracle-java7-installer']
 }
 
 
 class grails($version, $destination, $user = "root"){
-  exec {'install-gvm':
-    command => 'curl -s get.gvmtool.net | bash',
+
+  if !defined(File["$destination"]) {
+	file { "$destination":
+	  ensure => 'directory',
+	  owner => "$user",
+	  group => "$user",
+	  mode => '0755'
+	}
   }
 
-  exec {'source-gvm':
-    #dont know why we have to use bash -c
-    command => 'bash -c source ~/.gvm/bin/gvm-init.sh',  
+  if "$version" =~ /^1\.3\.[6-9]|^2\..*/ {
+	 $base_url = "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS"
   }
- 
-  exec { "install-grails":
-    command => "bash -c gvm install grails $version",
+  else {
+	 $base_url = "http://dist.codehaus.org/grails"
   }
- 
-  Exec['install-gvm'] -> Exec['source-gvm'] -> Exec['install-grails']
+  notify { "The base url for grails $version is '$base_url'": }
+
+  file { 'download-grails':
+	name          => "$destination/grails-$version.zip",
+	ensure        => "present",
+	source        => "/vagrant/software/grails-$version.zip",
+	owner         => "$user",
+	group         => "$user",
+	mode          => '0755',
+  }
+  #TODO remove the above file resource and replace with wget OR have an option for either
+ # exec{ "download-grails" :
+  #  command => "/usr/bin/wget -d --output-document=$destination/grails-$version.zip $base_url/grails-$version.zip",
+  #  before => Exec["unzip-grails"],
+  #}
+
+  exec{ "unzip-grails" :
+	cwd => "$destination",
+	command => "unzip grails-$version.zip",
+	creates => "$destination/grails-$version",
+  }
+
+  #exec{ "set-grails-paths" :
+  #    command => 'echo \'export GRAILS_OPTS="-server -noverify -Xshare:off -Xms512M -Xmx1536M -XX:PermSize=512M"\' > /etc/profile.d/grails_opts.sh',
+  #}
+
+  file { "/etc/profile.d/grails_variables.sh":
+	content => "export GRAILS_OPTS='-server -noverify -Xshare:off -Xmx1G -Xms256m -XX:MaxPermSize=512m';
+	export GRAILS_HOME='${destination}/grails-${version}';
+	PATH=\$PATH:\$GRAILS_HOME/bin:
+	",
+	ensure        => "present",
+	owner         => "$user",
+	group         => "$user",
+	mode          => '0755',
+  }
+
+  #PATH=$PATH:/new_path:/another_new_path
+  #export PATH
+  #Resource order
+  File['download-grails'] -> Exec['unzip-grails'] -> File['/etc/profile.d/grails_variables.sh']
 }
 
 node default {
+
 	include grailsStack
-  
+
 }
 
 
